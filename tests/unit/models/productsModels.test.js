f40388be261c260eb3f0cd8e77c059b40cbe3707
André Gross <andgross@gmail.com>
2023-01-20 15:08:57 -0300

    req 2 parcial

--- tests/unit/models/productsModels.test.js
+++ tests/unit/models/productsModels.test.js
@@ -6,41 +6,22 @@ const { productsMock } = require('./mocks/productsModels.mock');
 describe('Testes de unidade da camada Model', function () {
   describe('Recuperando a lista de produtos', function () {
 
-    // arranje
-    beforeEach(async function () {
-      sinon.stub(connection, 'execute').resolves(productsMock);
-    });
-
-    afterEach(async function () {
-      connection.execute.restore();
-    });
-
-    it('Retorna um array com todos os produtos', async function () {
-      // act
+    it('Recuperando a lista de produtos', async function () {
+      // Arrange
+      sinon.stub(connection, 'execute').resolves([productsMock]);
+      // Act
       const result = await productsModel.getAllProducts();
-        expect(result).to.be.an('array');
+      // Assert
+      expect(result).to.be.deep.equal(productsMock);
     });
 
-      it('Retorna todos os produtos', async function () {
-        const response = await productsModel.getAllProducts();
-        expect(response).to.be.equal(productsMock)
-      });
-  });
-
-  describe('Recuperando um produto a partir do seu id', () => {
-
-    // arranje
-    before(async () => {
-      sinon.stub(connection, 'execute').resolves([[[productsMock[0]]]]);
-    });
-
-    after(async () => {
-      connection.execute.restore();
-    });
-
-    it('Retorna o primeiro produto encontrado', async () => {
+    it('Recuperando um produto a partir do seu id', async function () {
+      // Arrange
+      sinon.stub(connection, 'execute').resolves([[productsMock[0]]]);
+      // Act
       const result = await productsModel.getProductsById(1);
-      expect(result).to.be.deep.equal(productsMock[0])
+      // Assert
+      expect(result).to.be.deep.equal(productsMock[0]);
     });
   });
 });
--- tests/unit/services/productsServices.test.js
+++ tests/unit/services/productsServices.test.js
@@ -6,57 +6,44 @@ const { productsMock, oneProductMock } = require('./mock/productsServices.mock')
 
 
 describe('Testes de unidade da camada Service', () => {
-  describe('listagem de produtos', () => {
-
-    //arrange
-    before(async () => {
+  describe('listagem de produtos', function () {
+    it('retorna a lista completa de produtos', async function () {
+      // arrange
       sinon.stub(productsModel, 'getAllProducts').resolves(productsMock);
-    });
 
-    after(async () => {
-      productsModel.getAllProducts.restore();
-    });
-
-    it('retorna null caso a lista esteja vazia', async () => {
       // act
       const result = await productsService.getAllProducts();
-      // assert
-      expect(result.type).to.be.equal(null);
-    });
 
-    it('Retorna a lista completa de produtos', async () => {
-      // act
-      const result  = await productsService.getAllProducts();
       // assert
-      expect(result.message).to.deep.equal(productsMock);
+      expect(result).to.deep.equal(productsMock);
     });
   });
 
-  describe('busca um produto pelo id', () => {
+  describe('busca de um produto por id', function () {
+    it('retorna um erro caso receba um ID inválido', async function () {
 
-    // arrange
-    before(async () => {
-      sinon.stub(productsModel, 'getProductsById').resolves(oneProductMock);
-    });
-
-    after(async () => {
-      productsModel.getProductsById.restore();
-    });
-
-    it('verifica se o retorno é um objeto', async () => {
       // act
-      const result = await productsService.getProductsById(1);
+      const result = await productsService.getProductsById('la');
+
       // assert
-      expect(result).to.be.an('object');
+      expect(result.type).to.equal(404);
+      expect(result.message).to.equal('Product not found');
     });
 
-    it('verifica se o objeto retornado contém informações do produto buscado', async () => {
+    it('retorna o produto caso ID existe', async function () {
+      // arrange
+      sinon.stub(productsModel, 'getProductsById').resolves(productsMock[0]);
+
       // act
-      const result = await productsService.getProductsById(1);
+      const result = await passengerService.getProductsById(1);
+
       // assert
-      expect(result).to.be.deep.equal({ id: 1, name: "Martelo de Thor" });
+      expect(result.type).to.equal(null);
+      expect(result.message).to.equal(productsMock[0]);
     });
+
 
   });
+
 });
 
